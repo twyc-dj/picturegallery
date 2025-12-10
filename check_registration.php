acaf52588a8ee8d3dab0242ceb58588af16ec0ce
Bambamoussa <39279217+Bambamoussa@users.noreply.github.com>
2025-12-10 10:12:54 +0000

    SQL Injection

--- check_registration.php
+++ check_registration.php
@@ -11,33 +11,61 @@ include 'includes/navbar.html';
 if (isset ($_SESSION['username'])){
 	header('location: index.php');
 }
-else{
-	
-    $username = $_POST['username'];
-    $password = $_POST['pass'];
 
 
-    $sql1 = "SELECT users_username, users_password FROM users WHERE users_username = '{$username}' AND users_password = '{$password}'";
-    $sql2 = "INSERT INTO users (users_username, users_password) VALUES ('$username', '$password')";
 
-    $result1 = mysqli_query ($connection, $sql1) or die (mysqli_error ($connection));
-
-    if (mysqli_num_rows ($result1) == 0){
-        if (mysqli_query ($connection, $sql2)){
-            include 'includes/new_registration.php';
-        }
-        else{
-            include 'includes/error.php';
-            
-        }
-
-    }
-    else{
-        include 'includes/notregistered.php';
-    }
+if (!isset($_POST['username']) || !isset($_POST['pass'])) {
+    include 'includes/error.php';
+    exit();
 }
 
-mysqli_close ($connection);
+
+$username = $_POST['username'];
+$password = $_POST['pass'];
+
+ 
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+
+$sql1 = "SELECT users_password FROM users WHERE users_username = ?";
+$stmt1 = $connection->prepare($sql1);
+
+
+if (!$stmt1) {
+    die("Erreur préparation requête : " . $connection->error);
+}
+
+$stmt1->bind_param("s", $username);
+$stmt1->execute();
+$result1 = $stmt1->get_result();
+
+
+if ($result1->num_rows == 0) {
+
+
+    $sql2 = "INSERT INTO users (users_username, users_password) VALUES (?, ?)";
+    $stmt2 = $connection->prepare($sql2);
+
+    if (!$stmt2) {
+        die("Erreur préparation requête : " . $connection->error);
+    }
+
+    $stmt2->bind_param("ss", $username, $hashedPassword);
+
+    if ($stmt2->execute()) {
+        include 'includes/new_registration.php';
+    } else {
+        include 'includes/error.php';
+    }
+
+    $stmt2->close();
+
+} else {
+    include 'includes/notregistered.php';
+}
+
+$stmt1->close();
+mysqli_close($connection);
 
 include 'includes/footer.html';
 
